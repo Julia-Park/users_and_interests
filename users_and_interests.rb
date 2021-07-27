ae0bcbb981d50beb6b97aabcb17494b818226a56
Julia-Park <julia.ay.park@gmail.com>
2021-07-27 11:33:36 -0400

    Add skeleton files for views, update notes on main program

--- users_and_interests.rb
+++ users_and_interests.rb
@@ -4,3 +4,12 @@ require "tilt/erubis"
 require "yaml"
 
 user_list = YAML.load(File.read("/data/users.yml"))
+
+# home page should redirect to a page that lists all of the users' names from users.yml
+# each user name should be a link to a page for that user
+  # display email address, list of interests with a comma separating in between
+  # include menu at bottom of each users's page with a link to all the other users (not including current user)
+# application should have layout that has a summary of # of all current users and sum of their interests
+  # use helper method count_interests
+# add new user to users.yaml file and verify that the site updates accordingly
+
